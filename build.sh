6ed16bb6565b3d6638e818395626c7a681e29c88
Steph <Steph@itsalocke.com>
2018-04-08 09:22:36 +0100

    Checkout branches

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 ## Updates
 git submodule update --init --recursive
+git submodule foreach -q --recursive \
+  'git checkout \
+  $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+  
 hugo version
 
 ## Infrastructure
